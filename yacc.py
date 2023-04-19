a10f3d45b651891e4a720498f392851a36c61b99
dnoak <daniel.ortega.carvalho@gmail.com>
2023-04-18 21:47:39 -0400

    funcionando

--- yacc.py
+++ yacc.py
@@ -7,34 +7,34 @@ def recursive_state_machine(automata, code, depth=1, next_state='start'):
     global pos
     while pos < len(code):
         states = automatas[automata][next_state]
-        log(f"{spaces(depth)}❓\"{code[pos]}\" ⏩ {states}")
+        log_syntatic(f"{spaces(depth)}❓\"{code[pos]}\" ⏩ {states}")
         for state in states:
             if state[0] == '$':
                 if (code[pos]) == format_state(state):
-                    log(f"{spaces(depth+1)}🟢 \"{code[pos]}\" {state}", 'green')
+                    log_syntatic(f"{spaces(depth+1)}🟢 \"{code[pos]}\" {state}", 'green')
                     pos += 1
                     if state[1:] in automatas[automata].keys():
                         next_state = state[1:]
                         break
                     else:
-                        log(f"{spaces(depth)} ✔️ $ {state=}")
+                        log_syntatic(f"{spaces(depth)} ✔️ $ {state=}")
                         return True
             else:
-                log(f"{spaces(depth+1)}{'🔹'} ↘️ INICIO {state}")
+                log_syntatic(f"{spaces(depth+1)}{'🔹'} ↘️ INICIO {state}")
                 return_state = recursive_state_machine(format_state(state), code, depth+1)
                 if return_state:
                     if state[1:] in automatas[automata].keys():
                         next_state = state[1:]
-                        log(f"{spaces(depth)} * {state=}")
+                        log_syntatic(f"{spaces(depth)} * {state=}")
                         break
                     else: return True
         else:
             if '$' in states:
-                log(f"{spaces(depth)}{'🔸'} FIM {automata}")
+                log_syntatic(f"{spaces(depth)}{'🔸'} FIM {automata}")
                 return True
-            log(f"{spaces(depth)}❌ {state=}", 'red')
+            log_syntatic(f"{spaces(depth)}❌ {state=}", 'red')
             return False
-        if depth == 1: log('\n')
+        if depth == 1: log_syntatic('\n')
 
 def read_automatas(folder):
     with open(f'{folder}/types.json') as j: types_fsm = json.load(j)
@@ -42,7 +42,26 @@ def read_automatas(folder):
     with open(f'{folder}/lexical.json') as j: lexical_fsm = json.load(j)
     return types_fsm, lalg_fsm, lexical_fsm
 
+def remove_coments(code):
+    #pos = 0
+    for pos, char in enumerate(code):
+        jump = 0
+        if char == '{':
+            code[pos] = ''
+            while jump != -1:
+                if code[pos] == '{':
+                    jump += 1
+                if code[pos] == '}':
+                    jump -= 1
+                code[pos] = ''
+                pos += 1
+            code[pos] = ''
+    
+    print(''.join(code))
+    return ''.join(code)
+
 def format_code(code, lexical_fsm):
+    code = remove_coments(list(code))
     lexical_order = list(lexical_fsm.keys())[0:2]
     for order in lexical_order:
         for symbol in lexical_fsm[order]['start']:
@@ -53,14 +72,17 @@ def format_code(code, lexical_fsm):
                 if symbol[1:] in code[i].strip():
                     code[i] = code[i].replace(symbol[1:], f' {symbol} ')
             code = ' '.join(code)
+    code = code.replace('end.', 'end .')
     return (code.replace('$', '').replace('  ', ' ')+' $').split()
 
 def tokenization(code):
     global pos
     lex_keys = ['reserved_symbols_2char', 'reserved_symbols_1char', 'reserved_words']
     lex_tokens = sum([automatas[lex_key]['start'] for lex_key in lex_keys], [])
-    types_tokens = {'id': 'ident', 'int': 'numero_int', 'float': 'numero_real'}
+    types_tokens = {'id': 'ident', 'int': 'numero_int', 'float': 'numero_real', '.': '.'}
     tokenized_code = []
+    lexical_error = 0
+    print(f" ______Análise léxica______")
     for token_pos, token in enumerate(code[:-1]):
         if ('$'+token) in lex_tokens:
             tokenized_code.append(token)
@@ -74,26 +96,35 @@ def tokenization(code):
                         tokenized_code.append(ttoken_value)
                         break
             else:
-                print(f"Erro no token \"{token}\", posição {token_pos}")
+                log(f"| Erro: \"{token}\", posição: {token_pos}", 'red')
+                lexical_error += 1
+    result_color = 'green' if len(code)-lexical_error == len(code) else 'red'
+    log(f"| tokens corretos: {len(code)-lexical_error}/{len(code)}", result_color)
     return tokenized_code + ['$']
 
 spaces = lambda x: ''.join([f'|{s}|' if s==x-1 else '|   ' for s in range(x)])
 format_state = lambda x: x.split('#')[0][1:]
-log = lambda x, color=False: ...#print(f"{colors[color]}{x}{colors['end']}") if color else print(x)
+log_syntatic = lambda x, color=False: ...#print(f"{colors[color]}{x}{colors['end']}") if color else print(x)
+log = lambda x, color=False: print(f"{colors[color]}{x}{colors['end']}") if color else print(x)
 colors = {'green': '\033[92m', 'red': '\033[91m', 'yellow': '\033[93m', 'end': '\033[0m'}
 
 code = '''
-program ident;
+program programa;
 var teste : real;
 var id3 : real ;
 var id2, id42 : integer;
+{comentário`{} teste} {3} 
 
-procedure sdusu2282j2(ident: integer);
-begin
-    ident := ident + ident;
+procedure sdusu2282j2(arg1: integer; arg2: real);
+begin {outro comentário teste}
     begin
-        while ident <= ident+ident do
-        read(ident);
+        begin
+            ident := arg1 + arg1;
+            begin
+                while arg1 <= ident+ident do
+                read(argumentoou);
+            end;
+        end;
     end;
 end;
 
@@ -102,33 +133,36 @@ begin
     read(ident, ident );
     if ident <=ident +ident then
     begin
-        ident := ident + 123;
-        write ( ident ) ;
-        write (ident);
+        ident := var1 + 123;
+        write ( var2 ) ;
+        read (var4);
     end
-    else ident := ident + 123.321 ; 
+    else ident :=  +.321 ; 
 end ;
 
 begin 
     read(   ident);
     ident( ident);
-end 
+end.
 '''
 
 automata = 'programa'
-pos = 0
 
 types_fsm, lalg_fsm, lexical_fsm = read_automatas('automatas')
 automatas = types_fsm | lalg_fsm | lexical_fsm
 
+pos = 0
 formated_code = format_code(code, lexical_fsm)
-
 tokenized_code = tokenization(formated_code)
-#print(tokenized_code)
 
 pos = 0
 recursive_state_machine_result = recursive_state_machine(automata, tokenized_code)
-print(f"\n {'_'*22}\n| Chegou ao fim: {bool(recursive_state_machine_result)}")
-print(f"| Tokens lidos: {pos}/{len(formated_code)-1}")
-print(f"| Último token: {tokenized_code[pos]} ({formated_code[pos]})\n {'‾'*22}")
+
+color_end = 'green' if recursive_state_machine_result else 'red'
+color_read = 'green' if pos == len(formated_code)-1 else 'red'
+color_last = 'green' if tokenized_code[pos] == '$' else 'red'
+print(f"\n _____Análise sintática_____")
+log(f"| Chegou ao fim: {bool(recursive_state_machine_result)}", color_end)
+log(f"| Tokens lidos: {pos}/{len(formated_code)-1}", color_read)
+log(f"| Último token lido: {tokenized_code[pos]} ({formated_code[pos]})", color_last)
 
